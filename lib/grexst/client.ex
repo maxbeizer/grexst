65a5b8296796783fe7afdafe54511ba1f2dd3337
Max Beizer <max.beizer@gmail.com>
2015-11-22 22:33:52 -0600

    Basics of GH client

---
+++ lib/grexst/client.ex
@@ -0,0 +1,37 @@
+defmodule Grexst.Client do
+  use HTTPoison.Base
+  defstruct auth: nil, endpoint: "https://api.github.com/gists"
+  @user_agent [{"User-agent", "grexst"}]
+
+  def new(auth), do: %__MODULE__{auth: auth}
+
+  def process_response(response) do
+    status_code = response.status_code
+    body        = response.body
+
+    response = unless body == "", do: body |> JSX.decode!,
+    else: nil
+
+    if (status_code == 200), do: response,
+    else: {status_code, response}
+  end
+
+  def get_gists(client, page \\ 1) do
+    url = url(client.endpoint, page)
+    request(:get, url, client.auth)
+  end
+
+  defp url(endpoint, page), do: endpoint <> "?page=#{page}"
+
+  defp authorization_header(%{access_token: token}, headers) do
+    headers ++ [{"Authorization", "token #{token}"}]
+  end
+
+  defp request(method, url, auth, body \\ "") do
+    json_request(method, url, body, authorization_header(auth, @user_agent))
+  end
+
+  defp json_request(method, url, body \\ "", headers \\ [], options \\ []) do
+    request!(method, url, JSX.encode!(body), headers, options) |> process_response
+  end
+end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Grexst.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison, :exjsx]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,7 @@ defmodule Grexst.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{ :httpoison, "~> 0.7.3" },
+     { :exjsx,     "~> 3.0" }]
   end
 end
