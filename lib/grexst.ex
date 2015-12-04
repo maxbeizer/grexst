2ed84f07260979a1494c03d9045baab79742732c
Max Beizer <max.beizer@gmail.com>
2015-12-03 19:26:43 -0600

    Add application and agent

--- lib/grexst.ex
+++ lib/grexst.ex
@@ -1,7 +1,20 @@
 defmodule Grexst do
+  use Application
   alias Grexst.Client
 
+  def start(_,_) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(Grexst.MatchesAgent, [])
+    ]
+
+    Supervisor.start_link(children, [strategy: :one_for_one, name: __MODULE__])
+  end
+
   def main(output, term) do
+    Grexst.MatchesAgent.clear_matches
+    IO.puts 'starting'
     Client.new(%{access_token: System.get_env("GREXST_TOKEN")})
     |> Client.get_gists
     |> handle_gists
--- mix.exs
+++ mix.exs
@@ -14,7 +14,10 @@ defmodule Grexst.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison, :exjsx]]
+    [
+      applications: [:logger, :httpoison, :exjsx],
+      mod: {Grexst, []}
+    ]
   end
 
   # Dependencies can be Hex packages:
